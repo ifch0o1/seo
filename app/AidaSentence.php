c2f908258aad3a21299a1c7c2e68f5f360e2877d
Ivo Tanev <ivo.h.tanev@gmail.com>
2020-07-01 10:18:19 +0300

    initial commit

---
+++ app/AidaSentence.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class AidaSentence extends Model
+{
+    public function save(array $options = [])
+    {
+        // If no author has been assigned, assign the current user's id as the author of the post
+        if (!$this->author_id && Auth::user()) {
+            $this->author_id = Auth::user()->id;
+        }
+
+        parent::save();
+    }
+}
