fddb250c56c635f1228f84ef5eec1bcc0d50d072
Ivo Tanev <ivo.h.tanev@gmail.com>
2020-07-10 17:31:33 +0300

    updated middlewares

--- routes/web.php
+++ routes/web.php
@@ -27,20 +27,16 @@ Route::get('admin/aida-generator', "AidaGeneratorController")
 Route::post('api/aida_posts/generate', "AidaGeneratorController@generate")
     ->middleware('auth');
 
-Route::post('api/custom_python_test', 'KeywordCrapperController@custom')
-    ->middleware('auth');
+Route::post('api/custom_python_test', 'KeywordCrapperController@custom');
 
-Route::resource('api/keywords', "KeywordCrapperController")
-    ->middleware('auth');
+Route::resource('api/keywords', "KeywordCrapperController");
 
 Route::apiResource('api/industry', "API\IndustryController")
     ->middleware('auth');
 
-Route::post('api/push_python_words', 'KeywordCrapperController@push_python_words')
-    ->middleware('auth');
+Route::post('api/push_python_words', 'KeywordCrapperController@push_python_words');
 
-Route::get('api/keywords', 'KeywordCrapperController@get_api_handler')
-    ->middleware('auth');
+Route::get('api/keywords', 'KeywordCrapperController@get_api_handler');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
